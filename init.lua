80916282be6dbdee8ca3ff55569ff25d3ce68880
Christian Burmeister <mail@cburmeister.me>
2024-04-01 16:42:52 +0200

    Made 4 tab default. Made relativenumbers default.

---
+++ init.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+-------------------------------------- options ------------------------------------------
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- Numbers
+vim.wo.relativenumber = true
